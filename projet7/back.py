30ffcec519a7e4edc07e798ae225dbde83b832f7
Jerome <jerome.richard1210@gmail.com>
2024-08-16 17:28:12 +0200

    heroku 2

---
+++ projet7/back.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify
+import lightgbm as lgb
+import pandas as pd
+import joblib
+import requests
+from io import BytesIO
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define the URL to download the model and data from GitHub
+data_url = 'https://raw.githubusercontent.com/Jerome-R1210/Formation-OC/master/app_train_sample.csv?token=GHSAT0AAAAAACVYF4Q6YS547JYJ45SH7TWKZV3ONYA'
+model_url = 'https://github.com/Jerome-R1210/Formation-OC/raw/master/pipeline_p7.joblib'
+
+# Load the pre-trained LightGBM model from URL
+def load_model_from_url(url):
+    response = requests.get(url)
+    model = joblib.load(BytesIO(response.content))
+    return model
+
+# Load the model
+model = load_model_from_url(model_url)
+
+# Function to fetch client data based on client_id
+def get_client_data(client_id):
+
+    # Load the client data from CSV
+    client_data = pd.read_csv(data_url)
+    
+    # Filter the client data based on client_id
+    client_data = client_data[client_data['SK_ID_CURR'] == int(client_id)]
+    
+    # Check if the client data is found
+    if client_data.empty:
+        return None
+    
+    # Drop the unnecessary columns
+    client_data = client_data.drop(columns=['TARGET'])
+    
+    return client_data
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get the client_id from the request
+        client_id = request.json.get('client_id')
+        
+        if client_id is None:
+            return jsonify({'error': 'No client_id provided'}), 400
+
+        # Fetch the client data
+        client_data = get_client_data(client_id)
+        
+        if client_data is None:
+            return jsonify({'error': f'No data found for client_id {client_id}'}), 404
+        
+        # Predict using the model
+        prediction_proba = model.predict_proba(client_data)[:, 1][0]
+
+        # Return the decision as a JSON response
+        return jsonify({'client_id': client_id,'prediction': prediction_proba})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
+
---
+++ projet7/front.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import requests
+
+# URL de votre API Flask
+API_URL = 'https://whispering-tundra-39675-b773c5580ef7.herokuapp.com/'
+
+# Fonction pour faire une requête POST à l'API Flask
+def predict_loan_approval(client_id):
+    try:
+        response = requests.post(API_URL, json={'client_id': client_id})
+        
+        if response.status_code == 200:
+            result = response.json()
+            prediction_proba = result.get('prediction')
+            return prediction_proba
+        else:
+            st.error(f"Erreur lors de la prédiction : {response.json()['error']}")
+            return None
+        
+    except Exception as e:
+        st.error(f"Erreur lors de la requête à l'API : {str(e)}")
+        return None
+
+# Code principal de l'application Streamlit
+def main():
+    st.title("Simulation de scoring client pour l'acceptation de prêt")
+    
+    # Saisie de l'identifiant client
+    client_id = st.text_input("Entrez l'identifiant du client:")
+    
+    if st.button("Prédire"):
+        if client_id:
+            try:
+                client_id = int(client_id)  # Convertir l'entrée en entier
+                st.write(f"Identifiant client saisi : {client_id}")
+                
+                # Appel à l'API Flask pour obtenir la probabilité
+                prediction_proba = predict_loan_approval(client_id)
+                
+                if prediction_proba is not None:
+                    st.write(f"Prédiction (probabilité de défaut) : {prediction_proba:.2f}")
+                    if prediction_proba > 0.5:
+                        st.write("Décision : Crédit accordé")
+                    else:
+                        st.write("Décision : Crédit refusé")
+                    
+            except ValueError:
+                st.error("Veuillez entrer un identifiant client valide (nombre entier).")
+
+# Exécuter l'application Streamlit
+if __name__ == "__main__":
+    main()
